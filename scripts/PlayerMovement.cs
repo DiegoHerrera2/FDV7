31c5fb95645d3935388e0326f9ab2735539ed89b
Diego-Herrera-alu0101408903 <72274410+DiegoHerrera2@users.noreply.github.com>
2024-10-19 21:11:30 +0100

    Blendtree

--- scripts/PlayerMovement.cs
+++ scripts/PlayerMovement.cs
@@ -9,16 +9,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Animator animator;
     private int _lastMoveX;
-    private State _state = State.IdleDown;
+    private State _state = State.Idle;
     
     private enum State
     {
-        IdleUp,
-        IdleDown,
-        IdleSide,
-        WalkUp,
-        WalkDown,
-        WalkSide
+        Idle,
+        Walk
     }
 
     // Update is called once per frame
@@ -29,64 +25,25 @@ public class PlayerMovement : MonoBehaviour
         
         var velocity = new Vector3(x, y, 0).normalized * (speed * Time.deltaTime);
         transform.Translate(velocity);
-
+        animator.SetFloat("Horizontal", x);
+        animator.SetFloat("Vertical", y);
         switch (_state)
         {
-            case State.IdleUp:
-            case State.IdleDown:
-            case State.IdleSide:
+            case State.Idle:
                 animator.SetBool("isWalking", false);
-                if (x != 0)
+                if (x != 0 || y != 0)
                 {
-                    
-                    _state = State.WalkSide;
-                }
-                else if (y > 0)
-                {
-                    _state = State.WalkUp;
-                }
-                else if (y < 0)
-                {
-                    _state = State.WalkDown;
+                    _state = State.Walk;
                 }
                 break;    
-            case State.WalkUp:
+            case State.Walk:
                 animator.SetBool("isWalking", true);
-                animator.SetInteger("moveY", 1);
-                if (y == 0)
-                {
-                    _state = State.IdleUp;
-                }
-                else if (y < 0)
-                {
-                    _state = State.WalkDown;
-                }
-                break;
-            case State.WalkDown:
-                animator.SetBool("isWalking", true);
-                animator.SetInteger("moveY", -1);
-                if (y == 0)
-                {
-                    _state = State.IdleDown;
-                }
-                else if (y > 0)
-                {
-                    _state = State.WalkUp;
-                }
-                break;
-            case State.WalkSide:
                 spriteRenderer.flipX = _lastMoveX > 0;
-                animator.SetBool("isWalking", true);
-                animator.SetInteger("moveY", 0);
-                if (x == 0)
-                {
-                    _state = State.IdleSide;
-                }
-                else if (y != 0)
-                {
-                    _state = y > 0 ? State.WalkDown : State.WalkUp;
-                }
                 _lastMoveX = x > 0 ? 1 : -1;
+                if (x == 0 && y == 0)
+                {
+                    _state = State.Idle;
+                }
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
